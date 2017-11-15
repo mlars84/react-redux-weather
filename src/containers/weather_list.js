f89f7df71d9ba99188ddf67e61fb794a0dd1b7b2
Matt Larson <matt.a.larson@gmail.com>
2017-11-15 14:13:11 -0600

    getting sparklines graphs to work

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import {Sparklines, SparklinesLine } from 'react-sparklines'
 
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name
-    // const temps = cityData.list.map(weather => weather.main.temp)
-    // const humidity = cityData.list.map(weather => weather.main.humidity)
-    // const pressure = cityData.list.map(weather => weather.main.pressure)
+    const temps = cityData.list.map(weather => weather.main.temp)
+    const humidity = cityData.list.map(weather => weather.main.humidity)
+    const pressure = cityData.list.map(weather => weather.main.pressure)
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>{temps}</td>
+        <td>
+          <Sparklines height={120} width={180} data={temps}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
+        <td>
+        <Sparklines height={120} width={180} data={humidity}>
+          <SparklinesLine color="blue" />
+        </Sparklines>
+      </td>
+      <td>
+      <Sparklines height={120} width={180} data={pressure}>
+        <SparklinesLine color="green" />
+      </Sparklines>
+    </td>
       </tr>
     )
   }
