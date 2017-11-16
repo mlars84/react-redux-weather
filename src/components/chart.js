e9fe995b60de82bc669262acd247bc147d1031f8
Matt Larson <matt.a.larson@gmail.com>
2017-11-15 19:15:30 -0600

    added average to averages

--- src/components/chart.js
+++ src/components/chart.js
@@ -14,7 +14,7 @@ export default (props) => {
         <SparklinesReferenceLine type="avg" />
         <SparklinesSpots />
       </Sparklines>
-      <div>{average(props.data)} {props.units}</div>
+      <div>Average: {average(props.data)} {props.units}</div>
     </div>
   )
 }
